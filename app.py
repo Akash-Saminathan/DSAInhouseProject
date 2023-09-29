774c64f0445b687bc5245e6c35213b82429e0e34
pkp-166 <123925482+pkp-166@users.noreply.github.com>
2023-09-29 23:12:43 +0530

    Attempted Adding the xread function to code

--- app.py
+++ app.py
@@ -2,9 +2,43 @@ from flask import Flask, render_template, url_for, request, send_file
 import pandas as pd
 from io import BytesIO 
 from reportlab.pdfgen import canvas
+import openpyxl
+import csv
+from openpyxl import Workbook,load_workbook
+
 
 
 app = Flask(__name__)
+
+#Adding File Reading Function:
+def xread(path):
+
+    path=path.strip()
+
+    if (path[-3:]=="csv"):
+        with open(path, 'r') as f:
+            reader = csv.reader(f)
+            metadata = list(reader)
+        return metadata
+
+    elif (path[-4:]=="xlsx"):
+        xl=load_workbook(path)
+        sheet=xl.active
+        maxrow=sheet.max_row
+        maxcol=sheet.max_column
+        metadata=[]
+        mtdt=[]
+
+        for j in range(1, maxrow + 1): #to replace into column wise data set.. change to maxrow+1 to maxcol+1
+            for i in range(1, maxcol + 1): #vice versa
+                cell = sheet.cell(row=j, column=i) #switch i and j
+                mtdt.append(cell.value)
+            metadata.append(mtdt)
+            mtdt=[]
+        return metadata
+
+    else:
+        return "Error. File not found or Invalid extension type. Please recheck path and use only csv or xlsx files."
 
 
 
